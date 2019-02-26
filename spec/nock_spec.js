352f8f1d52590058e825d805a923c7a081723a71
Filipe A Moreira <code@filipeamoreira.com>
2019-02-26 11:11:50 +0000

    Works with fetch-mock

--- spec/nock_spec.js
+++ spec/nock_spec.js
@@ -1,31 +1,26 @@
 const nock = require('nock');
-const request = require('request-promise-native');
+const fetchMock = require('fetch-mock');
+import 'whatwg-fetch';
 
 describe('nock', () => {
-  it('is setup correctly', (done) => {
-    nock.disableNetConnect();
-    nock('https://my-json-server.typicode.com')
-      .get('/typicode/demo/posts')
-      .reply(200, [
-        {
-          "id": 20,
-          "title": "Post 20"
-        },
-        {
-          "id": 21,
-          "title": "Post 21"
-        },
-      ]);
+  it('is setup correctly', async (done) => {
+    fetchMock.mock('https://my-json-server.typicode.com/typicode/demo/posts', [
+      { id: 20 }
+    ]);
 
-    request({
-      method: 'GET',
-      uri: 'https://my-json-server.typicode.com/typicode/demo/posts',
-      json: true
-    })
-      .then((posts) => {
-        expect(posts[0].id).toBe(20);
+    window.fetch('https://my-json-server.typicode.com/typicode/demo/posts')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        expect(data[0].id).toBe(20);
         done();
+      })
+      .catch((error) => {
+        console.log(error);
       });
+
+    fetchMock.reset();
   });
 });
 
